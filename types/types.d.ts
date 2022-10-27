9b981ab801c24aa7ac67c446b5bd31415df1ef83
Allan Jardine <allan@datatables.net>
2022-10-27 15:14:21 +0000

    Update typing information to latest typing integration for DataTables

---
+++ types/types.d.ts
@@ -0,0 +1,21 @@
+
+// Dist-DataTables-RowReorder-SemanticUI integration with SemanticUI exports the DataTables API having
+// set default values to complete the ingeration.
+import Api from "datatables.net";
+
+export default Api;
+
+
+// Dist-DataTables-RowReorder-SemanticUI integration with SemanticUI exports the DataTables API having
+// set default values to complete the ingeration.
+import Api from "datatables.net";
+
+export default Api;
+
+
+// Dist-DataTables-RowReorder-SemanticUI integration with SemanticUI exports the DataTables API having
+// set default values to complete the ingeration.
+import Api from "datatables.net";
+
+export default Api;
+
