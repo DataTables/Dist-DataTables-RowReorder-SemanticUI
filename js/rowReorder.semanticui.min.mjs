7dfebb61ccd8a3c42d0865514b75245e87c05ec9
Allan Jardine <allan@datatables.net>
2022-11-04 14:53:43 +0000

    Missing files in DataTables-DataTables

--- js/rowReorder.semanticui.min.mjs
+++ js/rowReorder.semanticui.min.mjs
@@ -1,4 +1,1 @@
-/*! Semanic UI styling wrapper for RowReorder
- * © SpryMedia Ltd - datatables.net/license
- */
 import $ from"jquery";import DataTable from"datatables.net-se";import DataTable from"datatables.net-rowreorder";export default DataTable;
