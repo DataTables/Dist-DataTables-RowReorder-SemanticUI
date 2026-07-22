f68459335a4237d50e1f08cf357e26085e0c0332
dtbuild <dtbuild@rigil>
2026-07-22 14:45:42 +0000

    289b8c5e08f85e8944b15864bf359daa7a582c95 Release 2.0.0 ready
    Sync to source repo @289b8c5e08f85e8944b15864bf359daa7a582c95

--- js/rowReorder.semanticui.mjs
+++ js/rowReorder.semanticui.mjs
@@ -1,4 +1,4 @@
-/*! RowReorder Semantic UI styling 2.0.0-beta.1 for DataTables
+/*! RowReorder Semantic UI styling 2.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
